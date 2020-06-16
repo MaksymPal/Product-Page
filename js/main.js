ece1bff4060fbfdd05a1180eafd89c9c29e4c26a
MaksymPal <tal4me777@gmail.com>
2020-06-16 13:59:46 +0300

    Add 'touchend' event

--- js/main.js
+++ js/main.js
@@ -2,19 +2,27 @@ const menuLink = document.querySelector('.menu-icon');
 const header = document.querySelector('.header');
 const gridItem = document.querySelectorAll('.page__grid-item');
 
-menuLink.addEventListener('click', function (event) {
+menuLink.addEventListener('touchend', function (event) {
     header.classList.toggle('navigation--active');
     this.classList.toggle('menu-icon--active');
 });
 
+function gridSelect (index) {
+    for (let i = 0; i < gridItem.length; i++) {
+        if (i !== index) {
+            gridItem[i].classList.remove('page__grid-item--active');
+        }
+    };
+
+    gridItem[index].classList.add('page__grid-item--active');
+};
+
 gridItem.forEach((current, index) => {
     current.addEventListener('click', () => {
-        for (let i = 0; i < gridItem.length; i++) {
-            if (i !== index) {
-                gridItem[i].classList.remove('page__grid-item--active');
-            }
-        };
+        gridSelect(index);
+    });
 
-        gridItem[index].classList.add('page__grid-item--active');
-    })
+    current.addEventListener('touchend', () => {
+        gridSelect(index);
+    });
 })
